d6604a8bf97efa3e47cc0da0897f45d50fbddaf2
Павел <pasha.rod@mail.ru>
2015-01-24 19:55:30 +0300

    Update editor_configuration.rb

--- lib/editor_configuration.rb
+++ lib/editor_configuration.rb
@@ -1,5 +1,5 @@
 class EditorConfiguration < Configuration
   preference :enabled, :boolean, :default => true
   preference :current_editor, :string, :default => 'YUIRichEditor'
-  preference :ids, :text, :default => 'product_description page_body'
+  preference :ids, :text, :default => 'product_description page_body belarus_body'
 end
